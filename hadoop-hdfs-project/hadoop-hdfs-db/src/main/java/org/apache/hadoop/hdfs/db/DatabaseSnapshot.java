bd647f46048fa4a79e529f30a36de467ad13d816
Gang Liao <gangliao@cs.umd.edu>
2019-03-22 02:45:58 -0400

    add snapshot to db

---
+++ hadoop-hdfs-project/hadoop-hdfs-db/src/main/java/org/apache/hadoop/hdfs/db/DatabaseSnapshot.java
@@ -0,0 +1,18 @@
+package org.apache.hadoop.hdfs.db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Types;
+import java.util.ArrayList;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DatabaseSnapshot {
+  static final Logger LOG = LoggerFactory.getLogger(DatabaseSnapshot.class);
+
+}
