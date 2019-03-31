8aa31c77a8cc89c3b29572bf8febac0f2bc70611
Gang Liao <gangliao@cs.umd.edu>
2019-03-31 03:15:45 -0400

    code format

--- HdfsSchemaInDB.java
+++ HdfsSchemaInDB.java
@@ -3,14 +3,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
-import java.lang.System;
 
 public class HdfsSchemaInDB {
   private static HdfsSchemaInDB instance;
   private Connection connection;
   private String postgres = "jdbc:postgresql://localhost:5432/docker";
   private String cockroach = "jdbc:postgresql://localhost:26257/docker";
-	private String volt = "jdbc:voltdb://localhost:21212";
+  private String volt = "jdbc:voltdb://localhost:21212";
   private String username = "docker";
   private String password = "docker";
 
@@ -20,23 +19,23 @@ public class HdfsSchemaInDB {
       String env = System.getenv("DATABASE");
       Properties props = new Properties();
 
-			if (env.equals("VOLT")) {
-				Class.forName("org.voltdb.jdbc.Driver");
+      if (env.equals("VOLT")) {
+        Class.forName("org.voltdb.jdbc.Driver");
         this.connection = DriverManager.getConnection(volt);
-				url = volt; 
-			} else if (env.equals("COCKROACH")) {
-				Class.forName("org.postgresql.Driver");
-				props.setProperty("user", username);
-				props.setProperty("sslmode", "disable"); 
-				this.connection = DriverManager.getConnection(cockroach, props);
-				url = cockroach;
-			} else {
-				Class.forName("org.postgresql.Driver");
-				props.setProperty("user", username);
-				props.setProperty("password", password);
-				this.connection = DriverManager.getConnection(postgres, props);
-				url = postgres;
-			}
+        url = volt;
+      } else if (env.equals("COCKROACH")) {
+        Class.forName("org.postgresql.Driver");
+        props.setProperty("user", username);
+        props.setProperty("sslmode", "disable");
+        this.connection = DriverManager.getConnection(cockroach, props);
+        url = cockroach;
+      } else {
+        Class.forName("org.postgresql.Driver");
+        props.setProperty("user", username);
+        props.setProperty("password", password);
+        this.connection = DriverManager.getConnection(postgres, props);
+        url = postgres;
+      }
       System.out.println("HdfsSchemaInDB: [" + env + "] " + url);
     } catch (Exception ex) {
       System.err.println("Database Connection Creation Failed : " + ex.getMessage());
@@ -46,12 +45,13 @@ public class HdfsSchemaInDB {
 
     try {
       // create inode table in Postgres
-			String sql1 = "DROP TABLE inodes IF EXISTS;"
-							+ "DROP TABLE inode2block IF EXISTS;"
-							+ "DROP TABLE datablocks IF EXISTS;"
-							+ "DROP TABLE blockstripes IF EXISTS;"
-							+ "DROP TABLE block2storage IF EXISTS;"
-							+ "DROP TABLE storage IF EXISTS;";
+      String sql1 =
+          "DROP TABLE inodes IF EXISTS;"
+              + "DROP TABLE inode2block IF EXISTS;"
+              + "DROP TABLE datablocks IF EXISTS;"
+              + "DROP TABLE blockstripes IF EXISTS;"
+              + "DROP TABLE block2storage IF EXISTS;"
+              + "DROP TABLE storage IF EXISTS;";
       String sql2 =
           "CREATE TABLE inodes("
               + "   id bigint primary key, parent bigint, name varchar(30),"
@@ -81,7 +81,7 @@ public class HdfsSchemaInDB {
               + ");";
       Statement st = connection.createStatement();
       st.execute(sql1);
-			st.execute(sql2);
+      st.execute(sql2);
       st.close();
     } catch (SQLException ex) {
       System.err.println(ex.getMessage());
@@ -101,11 +101,11 @@ public class HdfsSchemaInDB {
     return instance;
   }
 
-	public static void main(String [] args) {
-		try {
-			HdfsSchemaInDB.getInstance();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+  public static void main(String[] args) {
+    try {
+      HdfsSchemaInDB.getInstance();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
