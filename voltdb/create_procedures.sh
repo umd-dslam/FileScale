65d1f91916a2b74bad613d6205d03a08868c1382
Gang Liao <gangliao@cs.umd.edu>
2019-04-01 18:07:55 -0400

    compile and load into voltdb

--- voltdb/create_procedures.sh
+++ voltdb/create_procedures.sh
@@ -1,13 +1,35 @@
+cat <<EOF
+============================================
+Removing the Exist Stored Procedures ...
+============================================
+EOF
+
 EXISTS_PRCEDURES=$(echo "show procedures" | sqlcmd | awk '{print $1}' | sed -n '/VoltDBStoredProcedureTest/,$p')
 
 for procedure in $EXISTS_PRCEDURES
 do
     echo "drop stored procedure: $procedure ... "
-    echo "DROP PROCEDURE $procedure" | sqlcmd;
+    echo "DROP PROCEDURE $procedure;" | sqlcmd;
 done
+
+cat <<EOF
+============================================
+Load Stored Procedures into VoltDB ...
+============================================
+EOF
+rm -rf *.class
+javac *.java
+jar cvf storedprocs.jar *.class
+echo "load classes storedprocs.jar;" | sqlcmd 
+
+cat <<EOF
+============================================
+Creating New Stored Procedures ...
+============================================
+EOF
 
 for procedure in "VoltDBStoredProcedureTest" "RemoveChild"
 do
     echo "create stored procedure: $procedure ... "
-    echo "CREATE PROCEDURE FROM CLASS $procedure" | sqlcmd;
+    echo "CREATE PROCEDURE FROM CLASS $procedure;" | sqlcmd;
 done
