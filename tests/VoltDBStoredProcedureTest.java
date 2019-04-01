312aa2bbbac2290937b27360451d49f9911d89f8
Gang Liao <gangliao@cs.umd.edu>
2019-04-01 14:24:29 -0400

    fix error

--- tests/VoltDBStoredProcedureTest.java
+++ tests/VoltDBStoredProcedureTest.java
@@ -1,28 +1,33 @@
-import org.voltdb.*;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
+import org.voltdb.*;
 
 // https://docs.voltdb.com/tutorial/Part5.php
 public class VoltDBStoredProcedureTest extends VoltProcedure {
 
-  public final SQLStmt sql1 = new SQLStmt("SELECT id FROM inodes WHERE id = ?;");
-  public final SQLStmt sql2 = new SQLStmt("SELECT id FROM inodes WHERE parent = ?;");
+  public final SQLStmt sql1 =
+      new SQLStmt(
+          "WITH RECURSIVE cte AS ("
+              + "	SELECT id, parent FROM inodes d WHERE id = ?"
+              + " UNION ALL"
+              + " SELECT d.id, d.parent FROM cte"
+              + " JOIN inodes d ON cte.id = d.parent"
+              + " )"
+              + " SELECT id FROM cte;");
+	public final SQLStmt sql2 = new SQLStmt("DELETE FROM inodes WHERE id = ?;");
 
-  public Long[] run(long id) throws VoltAbortException {
-    long child;
-    List<Long> childs = new ArrayList<>();
+  public long run(long id) throws VoltAbortException {
     voltQueueSQL(sql1, id);
     VoltTable[] results = voltExecuteSQL();
-    while (results != null && results.length > 0) {
-      for (int i = 0; i < results.length; ++i) {
-        for (int j = 0; j < results[i].getRowCount(); ++j) {
-          child = results[i].fetchRow(j).getLong("id");
-          voltQueueSQL(sql2, child);
-          childs.add(child);
-        }
-      }
-      results = voltExecuteSQL();
+
+    if (results[0].getRowCount() < 1) {
+      return -1;
     }
-    return childs.toArray(new Long[childs.size()]);
+    for (int i = 0; i < results[0].getRowCount(); ++i) {
+			voltQueueSQL(sql2, results[0].fetchRow(i).getLong(0));
+    }
+    voltExecuteSQL();
+
+    return 1;
   }
 }
