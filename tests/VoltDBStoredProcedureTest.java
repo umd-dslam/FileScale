c53181670c0400c6f16672dcf06c2042f4648f3e
Gang Liao <gangliao@cs.umd.edu>
2019-04-01 12:21:10 -0400

    fix error

--- tests/VoltDBStoredProcedureTest.java
+++ tests/VoltDBStoredProcedureTest.java
@@ -1,11 +1,14 @@
 import org.voltdb.*;
+import java.util.List;
+import java.util.ArrayList;
 
+// https://docs.voltdb.com/tutorial/Part5.php
 public class VoltDBStoredProcedureTest extends VoltProcedure {
 
   public final SQLStmt sql1 = new SQLStmt("SELECT id FROM inodes WHERE id = ?;");
   public final SQLStmt sql2 = new SQLStmt("SELECT id FROM inodes WHERE parent = ?;");
 
-  public long[] run(long id) throws VoltAbortException {
+  public Long[] run(long id) throws VoltAbortException {
     long child;
     List<Long> childs = new ArrayList<>();
     voltQueueSQL(sql1, id);
@@ -13,7 +16,7 @@ public class VoltDBStoredProcedureTest extends VoltProcedure {
     while (results != null && results.length > 0) {
       for (int i = 0; i < results.length; ++i) {
         for (int j = 0; j < results[i].getRowCount(); ++j) {
-          long child = res[i].fetchRow(j).getString("id");
+          child = results[i].fetchRow(j).getLong("id");
           voltQueueSQL(sql2, child);
           childs.add(child);
         }
