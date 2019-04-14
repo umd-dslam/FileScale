8f5b15ffcae08a76cfa71568e1eeac76042ff2ce
Gang Liao <gangliao@cs.umd.edu>
2019-04-14 12:44:00 -0400

    add SetDelegationKeys in script

--- voltdb/create_procedures.sh
+++ voltdb/create_procedures.sh
@@ -1,5 +1,5 @@
 ## declare an array variable
-declare -a VOLTDB_PROCEDURES=("VoltDBStoredProcedureTest" "SetDelegationKeys" "SetStringTable" "RemoveChild" "RemoveBlock" "RemoveAllBlocks" "InsertINode2Block" "InsertXAttrs")
+declare -a VOLTDB_PROCEDURES=("VoltDBStoredProcedureTest" "SetPersistTokens" "SetDelegationKeys" "SetStringTable" "RemoveChild" "RemoveBlock" "RemoveAllBlocks" "InsertINode2Block" "InsertXAttrs")
 
 cat <<EOF
 ============================================
