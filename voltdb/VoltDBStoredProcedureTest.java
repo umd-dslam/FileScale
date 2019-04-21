35c0ad529b3ea33cc56c6ffce4cb63c780fc8403
Gang Liao <gangliao@cs.umd.edu>
2019-04-21 16:51:58 -0400

    update

--- voltdb/VoltDBStoredProcedureTest.java
+++ voltdb/VoltDBStoredProcedureTest.java
@@ -3,7 +3,7 @@ import org.voltdb.*;
 // https://docs.voltdb.com/tutorial/Part5.php
 public class VoltDBStoredProcedureTest extends VoltProcedure {
 
-  public final SQLStmt sql = new SQLStmt("SELECT id, parent FROM inodes WHERE id = ?;");
+  public final SQLStmt sql = new SQLStmt("SELECT id FROM inodes WHERE id = ?;");
 
   public VoltTable[] run(long id) throws VoltAbortException {
     voltQueueSQL(sql, id);
