85f99a8646c4d079a93536a732ec094da5b128c5
Gang Liao <gangliao@cs.umd.edu>
2019-06-19 18:05:06 -0700

    [ENH] add getHeader stored procedure

--- hadoop-hdfs-project/hadoop-hdfs-db/src/main/java/org/apache/hadoop/hdfs/db/DatabaseINode.java
+++ hadoop-hdfs-project/hadoop-hdfs-db/src/main/java/org/apache/hadoop/hdfs/db/DatabaseINode.java
@@ -274,7 +274,38 @@ public class DatabaseINode {
   }
 
   public static long getHeader(final long id) {
-    return getAttribute(id, "header");
+    long res = 0;
+    try {
+      DatabaseConnection obj = Database.getInstance().getConnection();
+      String env = System.getenv("DATABASE");
+      if (env.equals("VOLT")) {
+        VoltTable[] results = obj.getVoltClient().callProcedure("GetHeader", id).getResults();
+        VoltTable result = results[0];
+        result.resetRowPosition();
+        while (result.advanceRow()) {
+          res = result.getLong(0);
+        }
+      } else {
+        Connection conn = obj.getConnection();
+        String sql = "SELECT header FROM inodes WHERE id = ?;";
+        PreparedStatement pst = conn.prepareStatement(sql);
+        pst.setLong(1, id);
+        ResultSet rs = pst.executeQuery();
+        while (rs.next()) {
+          res = rs.getLong(1);
+        }
+        rs.close();
+        pst.close();
+      }
+      Database.getInstance().retConnection(obj);
+    } catch (SQLException ex) {
+      System.err.println(ex.getMessage());
+    }
+
+    if (LOG.isDebugEnabled()) {
+      LOG.debug(attrName + " [GET]: (" + id + "," + result + ")");
+    }
+    return res;
   }
 
   public static long getPermission(final long id) {
