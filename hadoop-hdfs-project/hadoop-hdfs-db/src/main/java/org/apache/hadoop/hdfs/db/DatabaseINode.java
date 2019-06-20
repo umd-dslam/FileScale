59d4820a06b021610063f2e0160603b6be7521fe
Gang Liao <gangliao@cs.umd.edu>
2019-06-20 10:28:27 -0700

    [ENH] add getPermission stored procedure

--- hadoop-hdfs-project/hadoop-hdfs-db/src/main/java/org/apache/hadoop/hdfs/db/DatabaseINode.java
+++ hadoop-hdfs-project/hadoop-hdfs-db/src/main/java/org/apache/hadoop/hdfs/db/DatabaseINode.java
@@ -411,7 +411,38 @@ public class DatabaseINode {
   }
 
   public static long getPermission(final long id) {
-    return getAttribute(id, "permission");
+    long res = 0;
+    try {
+      DatabaseConnection obj = Database.getInstance().getConnection();
+      String env = System.getenv("DATABASE");
+      if (env.equals("VOLT")) {
+        VoltTable[] results = obj.getVoltClient().callProcedure("GetPermission", id).getResults();
+        VoltTable result = results[0];
+        result.resetRowPosition();
+        while (result.advanceRow()) {
+          res = result.getLong(0);
+        }
+      } else {
+        Connection conn = obj.getConnection();
+        String sql = "SELECT permission FROM inodes WHERE id = ?;";
+        PreparedStatement pst = conn.prepareStatement(sql);
+        pst.setLong(1, id);
+        ResultSet rs = pst.executeQuery();
+        while (rs.next()) {
+          res = rs.getLong(1);
+        }
+        rs.close();
+        pst.close();
+      }
+      Database.getInstance().retConnection(obj);
+    } catch (SQLException ex) {
+      System.err.println(ex.getMessage());
+    }
+
+    if (LOG.isDebugEnabled()) {
+      LOG.debug("permission [GET]: (" + id + "," + res + ")");
+    }
+    return res;
   }
 
   public static long getParent(final long id) {
@@ -455,7 +486,7 @@ public class DatabaseINode {
       DatabaseConnection obj = Database.getInstance().getConnection();
       String env = System.getenv("DATABASE");
       if (env.equals("VOLT")) {
-        VoltTable[] results = obj.getVoltClient().callProcedure("GetName", id).getResults();
+        VoltTable[] results = obj.getVoltClient().callProcedure("GetName, id).getResults();
         VoltTable result = results[0];
         result.resetRowPosition();
         while (result.advanceRow()) {
