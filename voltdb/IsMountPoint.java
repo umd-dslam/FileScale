4db910101d24dbc0ffcd4bb66272357b973695c2
Gang Liao <gangliao@cs.umd.edu>
2019-09-02 23:40:46 -0400

    Fix runtime error

--- voltdb/IsMountPoint.java
+++ voltdb/IsMountPoint.java
@@ -4,7 +4,7 @@ public class IsMountPoint extends VoltProcedure {
 
   public final SQLStmt sql = new SQLStmt("SELECT COUNT(*) FROM mount WHERE path = ?;");
 
-  public long run(String path) throws VoltAbortException {
+  public VoltTable[] run(String path) throws VoltAbortException {
     voltQueueSQL(sql, path);
     return voltExecuteSQL();
   }
--- voltdb/IsUnified.java
+++ voltdb/IsUnified.java
@@ -4,7 +4,7 @@ public class IsUnified extends VoltProcedure {
 
   public final SQLStmt sql = new SQLStmt("SELECT COUNT(*) FROM mount WHERE path STARTS WITH ?;");
 
-  public long run(String path) throws VoltAbortException {
+  public VoltTable[] run(String path) throws VoltAbortException {
     voltQueueSQL(sql, path);
     return voltExecuteSQL();
   }
