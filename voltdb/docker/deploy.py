13cf25fa7d8b6bfb01aeb3da2f83b2b92ce1e2f7
Gang Liao <gangliao@cs.umd.edu>
2019-09-27 12:14:26 -0400

    [ENH] VoltDB Synchronous Command Logging

--- voltdb/docker/deploy.py
+++ voltdb/docker/deploy.py
@@ -2,10 +2,19 @@
 
 import sys, os
 
+# Time frequency is specified in milliseconds and transaction frequency is specified as
+# the number of transactions. You can specify either or both types of frequency. If you
+# specify both, whichever limit is reached first initiates a write.
+
 deploymentText = """<?xml version="1.0"?>
 <deployment>
     <cluster hostcount="##HOSTCOUNT##" kfactor="##K##" />
     <httpd enabled="true"><jsonapi enabled="true" /></httpd>
+
+    <commandlog enabled="true" synchronous="true" >
+        <frequency time="2" transactions="10000"/>
+    </commandlog>
+    <snapshot frequency="1h" retain="12" prefix="prefix" enabled="false"/>
 </deployment>
 """
 
