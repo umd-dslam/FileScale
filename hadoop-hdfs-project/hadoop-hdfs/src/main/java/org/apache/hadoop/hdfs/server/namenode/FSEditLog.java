dd4c6efcfbc85aa780fe6ed9fe734f6aa32f51de
Gang Liao <gangliao@cs.umd.edu>
2020-02-20 23:39:18 +0800

    feat: add editlog rpc interface

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSEditLog.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSEditLog.java
@@ -117,7 +117,10 @@ import org.slf4j.LoggerFactory;
 
 import java.io.OutputStream;
 import java.io.ByteArrayOutputStream;
+import java.net.InetSocketAddress;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.ipc.RPC;
 import org.apache.hadoop.hdfs.server.namenode.FSImageFormatProtobuf.LoaderContext;
 import org.apache.hadoop.hdfs.server.namenode.FSImageFormatProtobuf.SaverContext;
 import org.apache.hadoop.hdfs.server.namenode.FsImageProto.FileSummary;
@@ -859,6 +862,10 @@ public class FSEditLog implements LogsPurgeable {
     r.writeDelimitedTo(out);
 
     byte[] data = out.toByteArray();
+    FSEditLogProtocol proxy = (FSEditLogProtocol) RPC.getProxy(
+      FSEditLogProtocol.class, FSEditLogProtocol.versionID,
+      new InetSocketAddress(nameNodeAddress, 10000), new Configuration());
+    proxy.logEdit(data);
   }
 
   /** 
@@ -954,10 +961,15 @@ public class FSEditLog implements LogsPurgeable {
     }
 
     INodeSection.INode r = buildINodeCommon(newNode)
-        .setType(INodeSection.INode.Type.DIRECTORY).setDirectory(b).build();
+      .setType(INodeSection.INode.Type.DIRECTORY).setDirectory(b).build();
     r.writeDelimitedTo(out);
 
     byte[] data = out.toByteArray();
+
+    FSEditLogProtocol proxy = (FSEditLogProtocol) RPC.getProxy(
+      FSEditLogProtocol.class, FSEditLogProtocol.versionID,
+      new InetSocketAddress(nameNodeAddress, 10000), new Configuration());
+    proxy.logEdit(data);
   }
 
   /** 
