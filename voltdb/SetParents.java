bca16efa7bb7bc3ef821b9ede73352b7d9d823e3
Gang Liao <gangliao@cs.umd.edu>
2020-06-11 08:47:34 +0800

    fix typo

--- voltdb/SetParents.java
+++ voltdb/SetParents.java
@@ -6,7 +6,7 @@ public class SetParents extends VoltProcedure {
   public final SQLStmt sql = new SQLStmt("UPDATE inodes SET parent = ? WHERE id = ?;");
 
   public long run(final Long ids[], final long parent) throws VoltAbortException {
-    if (int i = 0; i < ids.length; ++i) {
+    for (int i = 0; i < ids.length; ++i) {
       voltQueueSQL(sql, parent, ids[i]);
     }
     voltExecuteSQL();
