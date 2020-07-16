fa677dd44c249cede84bb3cb6c9cea2b3401ba74
Gang Liao <gangliao@cs.umd.edu>
2020-07-16 17:34:36 +0800

    fix: updatesubtree

--- voltdb/SetId.java
+++ voltdb/SetId.java
@@ -3,19 +3,34 @@ import org.voltdb.*;
 // https://docs.voltdb.com/tutorial/Part5.php
 public class SetId extends VoltProcedure {
 
-    "INSERT INTO inodes("
-    + "id, name, accessTime, modificationTime, permission, header, parent"
-    + ") SELECT "
-    + " ?, name, accessTime, modificationTime, permission, header, ?"
-    + " FROM inodes WHERE id = ?;");
-
-  public final SQLStmt sql2 = new SQLStmt("DELETE FROM inodes where id = ?;");
+    public final SQLStmt sql1 = new SQLStmt(
+      "SELECT id, name, accessTime, modificationTime, permission,"
+      + "header, parent from inodes WHERE id = ?;");
+  
+    public final SQLStmt sql2 = new SQLStmt("DELETE FROM inodes where id = ?;");
+  
+    public final SQLStmt sql3 = new SQLStmt("INSERT INTO inodes("
+      + "id, name, accessTime, modificationTime, permission, header, parent"
+      + ") VALUES (?, ?, ?, ?, ?, ?, ?);");
 
   public long run(final long old_id, final long new_id, final long new_parent) throws VoltAbortException {
-    voltQueueSQL(sql1, new_id, new_parent, old_id);
-    voltExecuteSQL();
+    voltQueueSQL(sql1, old_id);
+    VoltTable[] results = voltExecuteSQL();
+
     voltQueueSQL(sql2, old_id);
-    voltExecuteSQL();
+
+    for (int j = 0; j < results.length; ++j) {
+      for (int i = 0; i < results[j].getRowCount(); ++i) {
+        voltQueueSQL(sql3,
+          new_id,
+          results[j].fetchRow(i).getString(1),
+          results[j].fetchRow(i).getLong(2),
+          results[j].fetchRow(i).getLong(3),
+          results[j].fetchRow(i).getLong(4),
+          results[j].fetchRow(i).getLong(5),
+          new_parent);
+      }
+    }
     return 1;
   }
 }
