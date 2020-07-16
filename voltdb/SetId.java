99272b9e6fbdc0cd350538c878e44113fd09296f
Gang Liao <gangliao@cs.umd.edu>
2020-07-16 10:44:58 +0800

    fix: updateSubtree

--- voltdb/SetId.java
+++ voltdb/SetId.java
@@ -3,10 +3,18 @@ import org.voltdb.*;
 // https://docs.voltdb.com/tutorial/Part5.php
 public class SetId extends VoltProcedure {
 
-  public final SQLStmt sql = new SQLStmt("UPDATE inodes SET id = ?, parent = ?  WHERE id = ?;");
+    "INSERT INTO inodes("
+    + "id, name, accessTime, modificationTime, permission, header, parent"
+    + ") SELECT " +
+    + " ?, name, accessTime, modificationTime, permission, header, ?"
+    + " FROM inodes WHERE id = ?;");
+
+  public final SQLStmt sql2 = new SQLStmt("DELETE FROM inodes where id = ?;");
 
   public long run(final long old_id, final long new_id, final long new_parent) throws VoltAbortException {
-    voltQueueSQL(sql, new_id, new_parent, old_id);
+    voltQueueSQL(sql1, new_id, new_parent, old_id);
+    voltExecuteSQL();
+    voltQueueSQL(sql2, old_id);
     voltExecuteSQL();
     return 1;
   }
--- voltdb/UpdateSubtree.java
+++ voltdb/UpdateSubtree.java
@@ -4,8 +4,25 @@ import java.util.*;
 // https://docs.voltdb.com/tutorial/Part5.php
 public class UpdateSubtree extends VoltProcedure {
   public final SQLStmt sql1 = new SQLStmt("SELECT id FROM inodes WHERE parent = ?");
-  public final SQLStmt sql2 = new SQLStmt("UPDATE inodes SET id = id + ?, parent = ? WHERE id = ?;");
-  public final SQLStmt sql3 = new SQLStmt("UPDATE inodes SET id = id + ?, parent = parent + ? WHERE id = ?;");
+
+  public final SQLStmt sql2 = new SQLStmt(
+    "INSERT INTO inodes("
+    + "id, name, accessTime, modificationTime, permission, header, parent"
+    + ") SELECT "
+    + " id + ?, name, accessTime, modificationTime, permission, header, ?"
+    + " FROM inodes WHERE id = ?;");
+
+  public final SQLStmt sql3 = new SQLStmt(
+    "INSERT INTO inodes("
+    + "id, name, accessTime, modificationTime, permission, header, parent"
+    + ") SELECT "
+    + " id + ?, name, accessTime, modificationTime, permission, header, parent + ?"
+    + " FROM inodes WHERE id = ?;");
+
+  public final SQLStmt sql4 = new SQLStmt("DELETE FROM inodes where id = ?;");
+
+  // public final SQLStmt sql2 = new SQLStmt("UPDATE inodes SET id = id + ?, parent = ? WHERE id = ?;");
+  // public final SQLStmt sql3 = new SQLStmt("UPDATE inodes SET id = id + ?, parent = parent + ? WHERE id = ?;");
 
   public long run(final long dir_id, final long dest_id, final long new_parent) throws VoltAbortException {
     List<Long> set = new ArrayList<>();
@@ -30,10 +47,14 @@ public class UpdateSubtree extends VoltProcedure {
     voltQueueSQL(sql2, dest_id, new_parent, dir_id);
  
     // childs
-    for (Long child : set) {
-      voltQueueSQL(sql3, dest_id, dest_id, child);
+    for (i = 1; i < set.size(); i++) {
+      voltQueueSQL(sql3, dest_id, dest_id, set.get(i));
     }
+    voltExecuteSQL();
 
+    for (Long child : set) {
+      voltQueueSQL(sql4, child);
+    }
     voltExecuteSQL();
     return 1;
   }
